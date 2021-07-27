4944299333b15f25a61b801e49518c52c85b0c20
zshigrit <zshi.grit@gmail.com>
2021-07-26 22:21:09 -0400

    start it today

--- leaf_photosynthesis.f90
+++ leaf_photosynthesis.f90
@@ -1,5 +1,12 @@
 ! leaf photosynthesis coupled with energy balance
 program leaf_photosyn
-real, parameters :: 
+    implicit none
+    real :: vcmax25, jmax25, rd25 
+
+
+
+    vcmax25=60.0
+    jmax25 = 1.67 * vcmax25
+    rd25 = 0.015 * vcmax25
 
 end program leaf_photosyn
