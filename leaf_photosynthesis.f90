97f1a46ef1563da1ebdbc285accc2eed6da9532d
zshigrit <zshi.grit@gmail.com>
2021-07-26 22:58:50 -0400

    all parameters for leafphotosynthesis

--- leaf_photosynthesis.f90
+++ leaf_photosynthesis.f90
@@ -3,7 +3,12 @@ program leaf_photosyn
     implicit none
     real :: vcmax25, jmax25, rd25
     real, parameter :: kc25= 404.9, ko25=278.4, cp25=42.75 
-    real, parameter :: kcha = 79430, koha = 36380, rdha = 46390, cpha = 37830, vcmaxha = 65330, jmaxha  = 43540  
+    real, parameter :: kcha = 79430.0, koha = 36380.0, rdha = 46390.0, cpha = 37830.0
+    real, parameter :: vcmaxha = 65330.0, jmaxha  = 43540.0
+    real, parameter :: rdhd = 150000.0, vcmaxhd = 150000.0, jmaxhd  = 150000.0    
+    real, parameter :: rdse = 490.0, vcmaxse = 490.0,jmaxse  = 490.0 
+    real, parameter :: phi_psii = 0.85, theta_j = 0.90, colim_c3 = 0.98
+    real, parameter :: rho= 0.10, tau= 0.10
 
     vcmax25=60.0
     jmax25 = 1.67 * vcmax25
