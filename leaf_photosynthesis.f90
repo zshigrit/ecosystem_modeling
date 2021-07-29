66d6790f91227a66f14af4a3defb9e6fc5d1c1e4
zshi0609@gmail.com <zshi.grit@gmail.com>
2021-07-29 00:35:25 -0400

    add Arrhenius equation

--- leaf_photosynthesis.f90
+++ leaf_photosynthesis.f90
@@ -20,6 +20,7 @@ program leaf_photosyn
     rdc    = fth25(rdhd, rdse)
 
     print *, vcmaxc, jmaxc, rdc
+    
 contains
 real function fth25(hd,se)
 
@@ -28,7 +29,30 @@ real function fth25(hd,se)
     real :: hd, se 
 
     fth25 = 1 + exp((-hd + se*(tfrz+25)) / (rgas*(tfrz+25)))
+
 end function fth25
+
+real function ft(tl,ha)
+
+    implicit none
+    real, parameter :: tfrz=273.15, rgas = 8.31446
+    real :: tl, ha 
+
+    ft = exp(ha/(rgas*(tfrz+25)) * (1-(tfrz+25)/tl));
+
+end function ft 
+
+real function fth(tl,hd,se,fc)
+    implicit none
+    real, parameter :: tfrz=273.15, rgas = 8.31446
+    real tl,hd,se,fc
+
+    fth = fc / (1 + exp((-hd+se*tl)/(rgas*tl)))
+
+end function fth 
+
+
+
 
 end program leaf_photosyn
 
