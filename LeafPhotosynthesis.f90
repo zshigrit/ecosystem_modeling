1c52e771f7700696aa278bb2c9f1e5c659ca4fc9
zshigrit <zshi.grit@gmail.com>
2021-08-03 10:24:21 -0400

    test

--- LeafPhotosynthesis.f90
+++ LeafPhotosynthesis.f90
@@ -1,12 +1,14 @@
 module LeafPhotosynthesis
     implicit none 
+ 
+    real, private, parameter :: tfrz=273.15, rgas = 8.31446
 
 contains
 
 real function fth25(hd,se)
 
     implicit none
-    real, parameter :: tfrz=273.15, rgas = 8.31446
+    
     real :: hd, se 
 
     fth25 = 1 + exp((-hd + se*(tfrz+25)) / (rgas*(tfrz+25)))
@@ -16,7 +18,7 @@ end function fth25
 real function ft(tl,ha)
 
     implicit none
-    real, parameter :: tfrz=273.15, rgas = 8.31446
+    
     real :: tl, ha 
 
     ft = exp(ha/(rgas*(tfrz+25)) * (1-(tfrz+25)/tl))
@@ -25,7 +27,7 @@ end function ft
 
 real function fth(tl,hd,se,fc)
     implicit none
-    real, parameter :: tfrz=273.15, rgas = 8.31446
+    
     real tl,hd,se,fc
 
     fth = fc / (1 + exp((-hd+se*tl)/(rgas*tl)))
